3eaa2ab4a50a3537bd9de06c1ae97a4ef39b3f9e
sairas774 <kurono_guren@yahoo.co.jp>
2021-05-19 13:33:12 +0900

    コメントなし　エラー修正

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :position, :occupation])
-  end
 end
